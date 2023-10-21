29fcbf4ac5d0ea643a37555b12313b30b06ca71d
Danielkozmo <danielkozmo@hotmail.es>
2023-10-21 01:50:49 -0500

    cambios en carrito

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const despliegueMenu = document.querySelector(".correo");
 const despliegue = document.querySelector(".desktopMenu");
 const menuHam = document.querySelector(".menu");
-const menuMobile = document.querySelector(".mobileMenu")
+const menuMobile = document.querySelector(".mobileMenu");
+const mCarrito = document.querySelector(".productDetail");
+const iconoCarrito = document.querySelector(".carrito");
 
+despliegueMenu.addEventListener("click", ff);
 menuHam.addEventListener("click",toggleMenu);
+iconoCarrito.addEventListener("click",toggleCard);
 
 function toggleMenu() {
     menuMobile.classList.toggle("inactive");    
 }
 
-despliegueMenu.addEventListener("click", function () {
-    despliegue.classList.toggle("inactive");    
-});
+function toggleCard() {
+    mCarrito.classList.toggle("inactive");    
+}
 
-    
+ function ff() {
+    despliegue.classList.toggle("inactive");    
+}
+
 
 
