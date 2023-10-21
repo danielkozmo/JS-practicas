0d579a78e8bebc82f0457fa48fafee5a07c6de95
Danielkozmo <danielkozmo@hotmail.es>
2023-10-21 00:31:07 -0500

    cambios en js empezando la primera innteraccion con el menu

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("holi");
+const despliegueMenu = document.querySelector(".correo");
+const despliegue = document.querySelector(".desktopMenu");
+let des = false;
+
+despliegueMenu.addEventListener("click", function () {
+    despliegue.classList.toggle("inactive");    
+});
+
+    
+
+
